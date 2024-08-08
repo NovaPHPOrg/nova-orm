57da2c93f733795ea7adb17c1dab9e2b4f1eeab3
Ankio <ankio@ankio.net>
2024-08-08 10:41:32 +0800

    :art: 优化字符串转义

--- object/Model.php
+++ object/Model.php
@@ -34,7 +34,7 @@ abstract class Model extends ArgObject
             $val = unserialize($val);
         }
 
-        if ($this->fromDb && is_string($demo) && !$this->inNofilter($key)) {
+        if ($this->fromDb && is_string($demo) && !$this->inNoEscape($key)) {
             if (empty($val)) {
                 $val = $demo;
             }
@@ -49,20 +49,20 @@ abstract class Model extends ArgObject
     }
 
     /**
-     * 是否为不不要过滤的字段
+     * 是否为不不要转义的字段
      * @param $key
      * @return bool
      */
-    private function inNoFilter($key): bool
+    private function inNoEscape($key): bool
     {
-        return in_array($key, $this->getNofilter());
+        return in_array($key, $this->getNoEscape());
     }
 
     /**
-     * 获取不需要过滤的字段
+     * 获取不需要转义的字段
      * @return array
      */
-    public function getNoFilter(): array
+    public function getNoEscape(): array
     {
         return [];
     }
