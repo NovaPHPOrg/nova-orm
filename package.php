a1b566cfd862aff8bf35f31767583a57263e1f09
ankio <ankio@ankio.net>
2024-11-06 19:13:54 +0800

    :wrench: (db): 添加数据库配置
    添加数据库配置，方便后续开发使用。

---
+++ package.php
@@ -0,0 +1,17 @@
+<?php
+
+;
+
+return [
+  "config"=>[
+      'db'=>[
+          'type'=>'mysql',
+          'host'=>'localhost',
+          'port'=>3306,
+          'username'=>'root',
+          'password'=>'root',
+          'db'=>'test',
+          'charset'=>'utf8mb4',
+      ],
+  ]
+];
