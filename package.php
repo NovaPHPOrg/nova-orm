0ab17b11c3ea80f92cb3b88c41fcb95f6e9da34d
ankio <ankio@ankio.net>
2025-07-27 13:48:20 +0800

    :wrench: (db): 添加数据库配置项
    在 ORM 插件中新增了数据库连接配置，支持 MySQL 数据库的基本连接参数设置，包括主机、端口、用户名、密码、数据库名和字符集。此更改使插件具备初始数据库连接能力。

--- package.php
+++ package.php
@@ -11,5 +11,16 @@
 declare(strict_types=1);
 
 return [
-
+    "config" => [
+        'db' =>
+            [
+                'type' => 'mysql',
+                'host' => '',
+                'port' => 3306,
+                'username' => '',
+                'password' => '',
+                'db' => '',
+                'charset' => 'utf8mb4',
+            ]
+    ]
 ];
